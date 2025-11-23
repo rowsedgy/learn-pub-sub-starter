7ec8a1a60f9955c442eb41012adfa89de78fb43c
rowsedgy <rowsedgy@gmail.com>
2025-11-23 14:32:04 +0100

    first channel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connString := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("error connecting to rabbitmq instance: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, string(routing.PauseKey), routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatalf("error pubsub: %v", err)
+	}
+	fmt.Println("Connection successful!")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Received signal")
+
 }
